848921e0a5964ab4ee2db005bad27d7351b84a5d
Eliub <eliubr47@gmail.com>
2022-04-27 21:29:54 -0500

    introduccion a java script

---
+++ ItemCelular.js
@@ -0,0 +1,6 @@
+//alert("Soy otro script");
+
+var nombre = "samuel";
+var edad = 30;
+
+Saludo(nombre,edad);
---
+++ Saludos.js
@@ -0,0 +1,7 @@
+function Saludo(name, edad){
+    if(edad == 31) {
+        console.log("Saludos para " + name);
+    }
+    else
+        console.log("No hay saludos");
+}
